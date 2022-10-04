1c3c6c445efaf13aba8687914ec7b9c0e44e88fa
Manoj Khanal <khanalmanoj26@gmail.com>
2022-10-04 19:42:36 +0545

    Second Commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,44 +1,20 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(
-    title: "Awesome App",
-    home: HomePage(),
-  ));
+  runApp(MyApp());
 }
 
-class HomePage extends StatelessWidget {
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text("Awesome App"),
-      ),
-      body: Container(),
-      drawer: Drawer(
-        child: ListView(
-          // ignore: prefer_const_literals_to_create_immutables
-          children: [
-            const DrawerHeader(
-              decoration: BoxDecoration(color: Colors.indigo),
-              child: Text(
-                "Hi I am a drawer",
-                style: TextStyle(color: Colors.white),
-              ),
-            ),
-            const ListTile(
-              leading: Icon(Icons.person),
-              title: Text("Account"),
-              subtitle: Text("Account"),
-              trailing: Icon(Icons.edit),
-            ),
-            const ListTile(
-              leading: Icon(Icons.email),
-              title: Text("Hello"),
-              subtitle: Text("helloworld@gmail.com"),
-              trailing: Icon(Icons.send),
-            ),
-          ],
+    return MaterialApp(
+      home: Material(
+        child: Center(
+          child: Container(
+            child: Text("Welcome to the flutter by codepur"),
+          ),
         ),
       ),
     );
